f060e97b7ab79e291133178b5932f03871731fa8
marindimitrov12 <marin200041@gmail.com>
2024-02-20 00:43:28 +0200

    add

---
+++ src/MoodNft.sol
@@ -0,0 +1,47 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity 0.8.18;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64}from"@openzeppelin/contracts/utils/Base64.sol";
+contract MoodNft is ERC721{
+    uint256 private s_tokenCounter;
+    string private s_sadSvgImageUri;
+    string private s_happySvgImageuri;
+
+
+    enum Mood{
+        HAPPY,
+        SAD
+    }
+    mapping(uint256=>Mood)private s_tokenIdtoMood;
+    constructor(string memory sadSvgImageUri,string memory happySvgImageUri) ERC721("MoodNft","MN"){
+
+        s_tokenCounter=0;
+        s_happySvg=happySvgImageUri;
+        s_sadSvg=sadSvgImageUri;
+    }
+    function mintNft()public{
+      _safeMint(msg.sender,s_tokenCounter);
+      s_tokenIdtoMood[s_tokenCounter]=Mood.HAPPY;
+      s_tokenCounter++;
+    }
+    function _baseURI()internal pure override returns (string memory){
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(uint256 tokenId)public view override returns (string memory){
+        string memory imageURI;
+        if(s_tokenIdtoMood[tokenId]==Mood.HAPPY){
+          imageURI=s_happySvgImageuri;
+        }else{
+            imageURI=s_sadSvgImageUri;
+        }
+        return string(
+       abi.encodePacked(
+        _baseURI
+        ,Base64.encode( bytes(abi.encodePacked('{"name":"',name(),'", "description:An nft that reflects the owners mood.", "attributes":[{"trait_type":"moodines","value":100}], "image":"',imageURI,'"  }')))))
+        ;
+    }
+
+}
---
+++ test/MoodNftTest.t.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity 0.8.18;
+
+import {Test}from "forge-std/Test.sol";
+import {MoodNft}from "../src/MoodNft.sol";
+
+contract MoodNftTest is Test{
+MoodNft moodNft;
+function setUp()public{
+
+}
+}
