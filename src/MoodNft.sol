220901e9c9e95252d86d1c1a9b04ac2716052e6d
marindimitrov12 <marin200041@gmail.com>
2024-02-20 16:24:42 +0200

    add deploy script and integration test for MoodNft.sol

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -1,10 +1,12 @@
 //SPDX-License-Identifier:MIT
 
-pragma solidity 0.8.18;
+pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64}from"@openzeppelin/contracts/utils/Base64.sol";
 contract MoodNft is ERC721{
+    //Errors
+    error MoodNft__CantFlipMoodIfNotOwner();
     uint256 private s_tokenCounter;
     string private s_sadSvgImageUri;
     string private s_happySvgImageuri;
@@ -18,13 +20,25 @@ contract MoodNft is ERC721{
     constructor(string memory sadSvgImageUri,string memory happySvgImageUri) ERC721("MoodNft","MN"){
 
         s_tokenCounter=0;
-        s_happySvg=happySvgImageUri;
-        s_sadSvg=sadSvgImageUri;
+        s_happySvgImageuri=happySvgImageUri;
+        s_sadSvgImageUri=sadSvgImageUri;
     }
     function mintNft()public{
       _safeMint(msg.sender,s_tokenCounter);
       s_tokenIdtoMood[s_tokenCounter]=Mood.HAPPY;
       s_tokenCounter++;
+    }
+    function flipMood(uint256 tokenId)public{
+        //only want the nft owner to be able to change the mood
+        if(_ownerOf(tokenId)!=msg.sender){
+            revert MoodNft__CantFlipMoodIfNotOwner();
+        }
+        if(s_tokenIdtoMood[tokenId]==Mood.HAPPY){
+          s_tokenIdtoMood[tokenId]=Mood.SAD;
+        }
+        else{
+            s_tokenIdtoMood[tokenId]=Mood.HAPPY;
+        }
     }
     function _baseURI()internal pure override returns (string memory){
         return "data:application/json;base64,";
@@ -39,7 +53,7 @@ contract MoodNft is ERC721{
         }
         return string(
        abi.encodePacked(
-        _baseURI
+        _baseURI()
         ,Base64.encode( bytes(abi.encodePacked('{"name":"',name(),'", "description:An nft that reflects the owners mood.", "attributes":[{"trait_type":"moodines","value":100}], "image":"',imageURI,'"  }')))))
         ;
     }
